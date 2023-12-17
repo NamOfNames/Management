bac1611992e1ccd612a52fd27f5b51036f9ccff6
nminhnghia <125782004+nminhnghia@users.noreply.github.com>
2023-12-17 11:28:30 +0700

    login and register

---
+++ src/Screen/Authentication/Register/main.js
@@ -0,0 +1,33 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { app } from "../../../../main.js"
+
+
+const eventFormLogin = document.querySelector(".formLogin")
+const inputEmail = document.querySelector(".inputEmail")
+const inputPassword = document.querySelector(".inputPassword")
+const auth = getAuth(app)
+
+console.log("auth", auth);
+
+
+eventFormLogin.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+      createUserWithEmailAndPassword(auth, inputEmail.value, inputPassword.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+  
+          alert("message success");
+          console.log(user);
+  
+          localStorage.setItem("accessToken", JSON.stringify(user.accessToken));
+          
+        })
+        .catch((error) => {
+          //   console.log(error.code, error.message);
+          if (error.code == "auth/email-already-in-use") {
+            alert("user đã tồn tại");
+          }
+          //   viết component toast
+        })
+    });
