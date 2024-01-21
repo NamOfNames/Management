eb66eeb1d0cee2dcec082469a9e3070a9d2aed6d
Hoang Tra <traneko37@gmail.com>
2024-01-21 11:52:37 +0700

    fix ui

---
+++ src/Screen/Home/main.js
@@ -0,0 +1,10 @@
+const iconHeart4 = document.querySelector('#icon-heart4');
+const iconHeart5 = document.querySelector('#icon-heart5');
+iconHeart4.addEventListener('click', () => {
+    iconHeart4.style.color= '#DC143C';
+    iconHeart4.style.transition = '.5s ease';
+});
+iconHeart5.addEventListener('click', () => {
+    iconHeart5.style.color= '#DC143C';
+    iconHeart5.style.transition = '.5s ease';
+});
